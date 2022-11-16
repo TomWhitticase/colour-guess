3ce71f7acba5dcd5a0ae9f539d0206e19e919a36
TomWhitticase <79092168+TomWhitticase@users.noreply.github.com>
2022-11-16 10:47:32 +0000

    bug fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -34,7 +34,12 @@ function shouldTextBeBlack(backgroundcolor: string): boolean {
 }
 
 function getRandomColor(): string {
-  return "#" + Math.floor(Math.random() * 16777215).toString(16);
+  return (
+    "#" +
+    Math.floor(Math.random() * 16777215)
+      .toString(16)
+      .padStart(6, "0")
+  );
 }
 
 function getColorAccuracy(color1: string, color2: string): number {
